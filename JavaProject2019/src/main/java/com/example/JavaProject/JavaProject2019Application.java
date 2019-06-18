7991b72166e4c0730b38623610f28d4e0773a3ac
Simone <S1081623@studenti.univpm.it>
2019-06-18 12:07:16 +0200

    Settimo commit
    Creazione progetto su Eclipse come Spring Starter Project

---
+++ JavaProject2019/src/main/java/com/example/JavaProject/JavaProject2019Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProject2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProject2019Application.class, args);
+	}
+
+}
