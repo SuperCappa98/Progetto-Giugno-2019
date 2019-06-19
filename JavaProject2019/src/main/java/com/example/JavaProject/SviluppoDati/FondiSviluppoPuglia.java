fb51976334ed0ba653abbb3dd7e4bdd8791f9d83
Simone <S1081623@studenti.univpm.it>
2019-06-19 18:39:55 +0200

    Update FondiSviluppoPuglia.java

--- JavaProject2019/src/main/java/com/example/JavaProject/SviluppoDati/FondiSviluppoPuglia.java
+++ JavaProject2019/src/main/java/com/example/JavaProject/SviluppoDati/FondiSviluppoPuglia.java
@@ -3,7 +3,7 @@ package com.example.JavaProject.SviluppoDati;
 import java.io.* ;
 
 public class FondiSviluppoPuglia implements Serializable {
-	private static final long serialVersionUID = 1;
+	private static final long serialVersionUID = 1L;
 	private String programma;
 	private String asse;
 	private String linea;
